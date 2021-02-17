3e103349c6b4b0cbba202fea818cc96ef30032a6
Safwan Hossain <hossam18@mcmaster.ca>
2021-02-16 20:09:55 -0500

    finished constructor and outer comments

--- A2/src/TriangleT.py
+++ A2/src/TriangleT.py
@@ -1,4 +1,27 @@
 ## @file TriangleT.py
-#  @author
-#  @brief
-#  @date
+#  @author Safwan Hossain
+#  @brief ADT for representing equilateral triangles
+#  @date 2/16/2021
+
+from Shape import Shape
+
+
+## @brief An ADT for handling equliateral triangle that inherits Shape.py
+# @details An equilateral triangle has an x and y coordinate,
+# non-zero (equal) side lengths, and a non-zero mass
+class TriangleT(Shape):
+    ## @brief constructor for TriangleT
+    #  @param x Float representing the x coordinate of the triangle
+    #  @param y Float representing the y coordinate of the triangle
+    #  @param s Float representing the side lengths of the triangle
+    #  @param m Float representing the mass of the triangle
+    #  @throws ValueError when either mass or side lengths are 0 or lower
+
+    def __init__(self, x, y, s, m):
+        if s <= 0 or m <= 0:
+            raise ValueError("mass and side lengths must be greater than zero")
+
+        self.x = x
+        self.y = y
+        self.s = s
+        self.m = m
