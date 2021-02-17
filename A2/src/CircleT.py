06a0ff5b6926e2d0e3add40719babe40c3af4dcc
Safwan Hossain <hossam18@mcmaster.ca>
2021-02-16 19:43:19 -0500

    finished constructor and some comments

--- A2/src/CircleT.py
+++ A2/src/CircleT.py
@@ -1,4 +1,23 @@
 ## @file CircleT.py
-#  @author
+#  @author Safwan Hossain, Hossam18
 #  @brief
-#  @date
+#  @date  2/16/2021
+from Shape import Shape
+
+
+class CircleT(Shape):
+    ## @brief constructor for CircleT
+    #  @param x Float representing the x coordinate of the circle
+    #  @param y Float representing the y coordinate of the circle
+    #  @param r Float representing the radius of the circle
+    #  @param r Float representing the mass of the circle
+    #  @throws ValueError when either mass or radius is 0 or lower
+
+    def __init__(self, x, y, r, m):
+        if r <= 0 and m <= 0:
+            raise ValueError("mass and radius should be greater than zero")
+
+        self.x = x
+        self.y = y
+        self.r = r
+        self.m = m
