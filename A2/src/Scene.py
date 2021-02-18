73e01dfa00f29bfe8189c0243cef911cb806ed51
Safwan Hossain <hossam18@mcmaster.ca>
2021-02-17 19:18:37 -0500

    test

--- A2/src/Scene.py
+++ A2/src/Scene.py
@@ -40,3 +40,42 @@ class Scene(Shape, SciPy):
     # @returns Float vy representing the initial y velocity of the shape
     def get_init_velo(self):
         return self.vx, self.vy
+
+    ## @brief sets the shape of the scene
+    # @param s the new shape of the scene
+    def set_shape(self, s):
+        self.s = s
+
+    ## @brief sets the forces of the scene
+    # @param fx the new x force of the scene
+    # @param fy the new y force of the scene
+    def set_unbal_forces(self, fx, fy):
+        self.fx = fx
+        self.fy = fy
+
+    ## @brief sets the velocities of the scene
+    # @param vx the new x velocity of the shape
+    # @param vy the new y velocity of the shape
+    def set_init_velo(self, vx, vy):
+        self.vx = vx
+        self.vy = vy
+
+    ## @brief
+    # @param t_final a Float value representing final time
+    # @param nsteps representing number of steps
+    # @return List containing Floats that represent time
+    # @reutrn List containing Floats that represent the solution.
+    def sim(self, t_final, nsteps):
+        t = []
+        for i in range * nsteps:
+            t.append((i * t_final) / (nsteps - 1))
+        return t, odeint(self.ode, [self.s.cm_x(), self.s.cm_y(), self.Vx, self.Vy], t)
+
+    ## @brief a method for finding the ode
+    # @details This method is used to find the ode more details about ode can
+    # be found in https://en.wikipedia.org/wiki/Ordinary_differential_equation
+    # @param w List containing Floats that represent rx, ry, vx and vy
+    # @param t List containing Floats that represent time
+    # @return List of Floats that represent the solution
+    def ode(self, w, t):
+        return [w[2], w[3], self.Fx(t) / self.s.mass(), self.Fy(t) / self.s.mass()]
