fdc6240bb40b43e9e2ac371251f0e8030deceba5
Safwan Hossain <hossam18@mcmaster.ca>
2021-03-04 19:29:31 -0500

    finished all functions

--- Midterm/src/SeqServicesLibrary.py
+++ Midterm/src/SeqServicesLibrary.py
@@ -1,6 +1,43 @@
 ## @file SeqServicesLibrary.py
-#  @author Your name
+#  @author Safwan Hossain, Hossam18, 400252391 
 #  @brief Library module that provides functions for working with sequences
 #  @details This library assumes that all functions will be provided with arguments of the expected types
 #  @date 03/04/2021
+
+def max_val(s):
+    if len(s) == 0:
+        raise ValueError("List is empty")
+
+    m = 0
+    for x in s:
+        if m < abs(x):
+            m = abs(x)
+
+    return m
+
+
+def count(t, s):
+    if len(s) == 0:
+        raise ValueError("List is empty")
+
+    sum = 0
+    for x in s:
+        if x == t:
+            sum += 1
+
+    return sum
+
+
+def spices(s):
+    if len(s) == 0:
+        raise ValueError("List is empty")
+
+    return ["nutmeg" if x <= 0 else "ginger" for x in s]
+
+
+def new_max_val(s, f):
+    if len(s) == 0:
+        raise ValueError("List is empty")
+
+    return max_val([f(x) for x in s])
 
