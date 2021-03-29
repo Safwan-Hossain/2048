c91c1518fc7e7231bf6edab272ee0cec6a6b1c38
Safwan Hossain <hossam18@mcmaster.ca>
2021-03-29 13:11:22 -0400

    finished attributeT

--- A3/src/AttributeT.java
+++ A3/src/AttributeT.java
@@ -1,2 +1,29 @@
 package src;
+import java.util.HashSet;
 
+public class AttributeT {
+    String name;
+    HashSet<IndicatorT> s;
+
+    public AttributeT(String attribName, IndicatorT[] indicators) {
+        this.name = attribName;
+        this.s = new HashSet<IndicatorT>();
+        for (IndicatorT indicator: indicators) {
+            s.add(indicator);
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public IndicatorT[] getIndicators() {
+        IndicatorT[] indicators = new IndicatorT[this.s.size()];
+        int i = 0;
+        for (IndicatorT indicator: this.s) {
+            indicators[i] = indicator;
+            i++;
+        }
+        return indicators;
+    }
+}
