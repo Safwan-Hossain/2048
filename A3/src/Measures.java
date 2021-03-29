53834840b967e15c9aa184d4e9b3724afa21b6cc
Safwan Hossain <hossam18@mcmaster.ca>
2021-03-29 13:31:25 -0400

    finished measures interface

--- A3/src/Measures.java
+++ A3/src/Measures.java
@@ -1,1 +1,15 @@
+/**
+ * @File: IndicatorT.java
+ * @Author: Safwan Hossain, Hossam18, 400252391
+ * @Date: March.29th, 2021
+ * @Description: 
+ */
+
 package src;
+public interface Measures {
+
+    double[] measures() throws UnsupportedOperationException;
+    double[] measures(IndicatorT ind) throws UnsupportedOperationException;
+    double[] measures(AttributeT att) throws UnsupportedOperationException;
+
+}
