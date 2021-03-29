8e5d670694cfe6c602b89e81c8bc2aa9acc65b05
Safwan Hossain <hossam18@mcmaster.ca>
2021-03-29 15:16:52 -0400

    finished LOsT

--- A3/src/LOsT.java
+++ A3/src/LOsT.java
@@ -1,1 +1,57 @@
+/**
+ * @File: LOsT.java
+ * @Author: Safwan Hossain, Hossam18, 400252391
+ * @Date: March.29th, 2021
+ * @Description: 
+ */
+
 package src;
+public class LOsT implements Measures{
+    String name;
+    int n_blw;
+    int n_mrg;
+    int n_mts;
+    int n_exc;
+
+    public LOsT(String topic, int nblw, int nmrg, int nmts, int nexc) {
+
+        if (nblw < 0 || nmts < 0 || nmrg < 0 || nexc < 0) {
+            throw new IllegalArgumentException("Numbers cannot be less than 0");
+        }
+        if (nblw == 0 && nmts == 0 && nmrg == 0 && nexc == 0) {
+            throw new IllegalArgumentException("All numbers cannot be 0");
+        }
+
+        this.name = topic;
+        this.n_blw = nblw;
+        this.n_mts = nmts;
+        this.n_mrg = nmrg;
+        this.n_exc = nexc;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean equals(LOsT o) {
+        return this.name.equals(o.getName());
+    }
+
+    @Override
+    public double[] measures() {
+        if (!Norm.getNLOs()) {
+            return new double[] { this.n_blw, this.n_mrg, this.n_mts, this.n_exc };
+        }
+        return Services.normal(new double[] { this.n_blw, this.n_mrg, this.n_mts, this.n_exc });
+    }
+
+    @Override
+    public double[] measures(IndicatorT ind) {
+        throw new UnsupportedOperationException("Invalid input");
+    }
+
+    @Override
+    public double[] measures(AttributeT att) {
+        throw new UnsupportedOperationException("Invalid input");
+    }
+}
