0d7102f5bd7f9bf2efbb3c95f8b0a9eb47e9acaa
Seefween <safwanh2708@gmail.com>
2021-04-28 22:25:21 -0400

    exam done

--- FinalExam/src/MeanCalculator.java
+++ FinalExam/src/MeanCalculator.java
@@ -1,1 +1,6 @@
 package src;
+import java.util.ArrayList;
+
+public interface MeanCalculator {
+    public Double meanCalc(ArrayList<Double> v);
+}
--- FinalExam/src/QuadraticMean.java
+++ FinalExam/src/QuadraticMean.java
@@ -1,1 +1,14 @@
 package src;
+import java.util.ArrayList;
+
+public class QuadraticMean implements MeanCalculator{
+
+    @Override
+    public Double meanCalc(ArrayList<Double> v) {
+        Double sum = 0.0;
+        for (Double x: v) {
+            sum += x * x;
+        }
+        return Math.sqrt(sum / v.size());
+    }
+}
